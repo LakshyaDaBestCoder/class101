ff5d72e516ec0ee7b05b121c15f83dd78340e66a
LakshyaDaBestCoder <95533381+LakshyaDaBestCoder@users.noreply.github.com>
2021-12-28 16:12:32 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,5 +1,6 @@
 import dropbox
 import os
+from dropbox.files import WriteMode
 
 class TransferData:
     def __init__(self, access_token):
@@ -8,14 +9,16 @@ class TransferData:
     def upload_file(self, file_from, file_to):
         dbx = dropbox.Dropbox(self.access_token)
         for root, dirs, files in os.walk(file_from):
-            relativePath= os.path.relpath(local_path, file_from)
-            dropboxPath= os.path.join(file_to, relativePath)
+            for fileName in files:
+                local_path=os.path.join(root,fileName)
+                relativePath= os.path.relpath(local_path, file_from)
+                dropboxPath= os.path.join(file_to, relativePath)
 
-            with open(local_path, 'rb') as f:
-            dbx.files_upload(f.read(), dropboxPath, mode=WriteMode("overwrite"))
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropboxPath, mode=WriteMode('overwrite'))
 
 def main():
-    access_token = 'sl.A-nHh9M7jiTJaM23I9s38ACR7c-Ymph_GYdDxuaeUkyOWTzIe9TuZvJQOYofWuQQfP20VvAlUUWVk-Wo3t38AhOsxTJLPM1PfKKD89VEpQeMLU5NBr7FqA2mRQhV9flYpn9yUdc'
+    access_token = '1qCHReJ21_0AAAAAAAAAAZmXz0ol-jWbkhzAzLA5WOJdQmD6ztYVcDLpaXBoV-Dp'
     transferData = TransferData(access_token)
 
     file_from = input("Insert the file_from")
